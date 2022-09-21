41430607cc5e11ca55d271745ae842c82ae30220
Rashad Muntar <muntartoure@gmail.com>
2022-09-21 15:28:39 +0000

    implemented fetch header options

--- index.ts
+++ index.ts
@@ -1,13 +1,34 @@
 import { useEffect, useState } from "react";
+import { fetProps } from "./types";
 
-const useDataFetch = (url:string) => {
+const useDataFetch = ({
+  url,
+  method,
+  mode,
+  cat,
+  credentials,
+  contentType,
+  redirect,
+  referrerPolicy,
+}: fetProps) => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-     const response = await fetch(url)
-        const data  = await response.json()
-        setData(data)
+      const response = await fetch(url, {
+        method: method, // *GET, POST, PUT, DELETE, etc.
+        mode: mode, // no-cors, *cors, same-origin
+        cache: cat, // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: credentials, // include, *same-origin, omit
+
+        headers: {
+          "Content-Type": "application/json" || contentType,
+        },
+        redirect: redirect, // manual, *follow, error
+        referrerPolicy: referrerPolicy, // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      });
+      const data = await response.json();
+      setData(data);
     };
     fetchData();
   }, []);
