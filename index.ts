461d2f0ae86468a192a2aa020d5e137a95899969
Rashad Muntar <muntartoure@gmail.com>
2022-09-24 01:21:56 +0000

    resolve typescript error

--- index.ts
+++ index.ts
@@ -1,59 +1,53 @@
 import { useEffect, useState } from "react";
-import { fetProps } from "./types";
 import {
-  methods,
-  modes,
-  cats,
-  credential,
-  contentTypes,
-  redirects,
-  referrerPolicies,
-} from "./optionValues";
-
-const useDataFetch = ({
-  url,
   method,
-  mode,
   cat,
+  mode,
   credentials,
   contentType,
   redirect,
   referrerPolicy,
-}: fetProps) => {
-  const [data, setData] = useState(null);
+} from "./types";
 
-  if (
-    methods.indexOf(method?.toString() as string) === -1 ||
-    modes.indexOf(mode?.toString() as string) === -1 ||
-    cats.indexOf(cat?.toString() as string) === -1 ||
-    credential.indexOf(credentials?.toString() as string) === -1 ||
-    contentTypes.indexOf(contentType?.toString() as string) === -1 ||
-    redirects.indexOf(redirect?.toString() as string) === -1 ||
-    referrerPolicies.indexOf(referrerPolicy?.toString() as string) === -1
-  ) {
-    return;
-  }
+const useDataFetch = (
+  url: string,
+  method?: method,
+  mode?: mode,
+  cat?: cat,
+  credentials?: credentials,
+  contentType?: contentType,
+  redirect?: redirect,
+  referrerPolicy?: referrerPolicy
+) => {
+  const [data, setData] = useState();
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(url, {
-        method: method, // *GET, POST, PUT, DELETE, etc.
-        mode: mode, // no-cors, *cors, same-origin
-        cache: cat, // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: credentials, // include, *same-origin, omit
-        headers: {
-          "Content-Type": "application/json" || contentType,
-        },
-        redirect: redirect, // manual, *follow, error
-        referrerPolicy: referrerPolicy, // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      });
-      const data = await response.json();
-      setData(data);
+      try {
+        setLoading(true);
+        const response = await fetch(url, {
+          method: method,
+          mode: mode,
+          cache: cat,
+          credentials: credentials,
+          headers: {
+            "Content-Type": "application/json" || contentType,
+          },
+          redirect: redirect,
+          referrerPolicy: referrerPolicy,
+        });
+        const data = await response.json();
+        setData(data);
+        setLoading(false);
+      } catch (err: any) {
+        setError(err.messages);
+      }
     };
     fetchData();
   }, []);
-
-  return data;
+  return { data, error, loading };
 };
 
 export default useDataFetch;
